70ab5cb54c54a96a060544190d4152180a13f32c
yashaspadma <yyashasy@gmail.com>
2025-03-26 17:47:58 +0530

    updated setup connection

--- src/pi_instruments/pi_intruments.py
+++ src/pi_instruments/pi_intruments.py
@@ -49,70 +49,29 @@ class pi_control:
 
 
     # Z movements
-    def pi_home_z(self):
+    def pi_Zhome(self):
         pi_control.send_command("N50 G01 Z0 F500")
 
-    def pi_z_minus(self):
+    def pi_ZM(self):
         self.send_command("N60 G01 Z-10 F500")
 
-    def move_z_plus(self):
+    def pi_ZP(self):
         self.send_command("N70 G01 Z10 F500")
 
     # XY movements
-    def home_xy(self):
+    def pi_XYhome(self):
         self.send_command("N70 G01 X0 Y0 F500")
 
-    def move_x_minus(self):
+    def pi_XM(self):
         self.send_command("N20 G01 X-10 F500")
 
-    def move_x_plus(self):
+    def pi_XP(self):
         self.send_command("N30 G01 X10 F500")
 
-    def move_y_minus(self):
+    def pi_YM(self):
         self.send_command("N40 G01 Y-10 F500")
 
-    def move_y_plus(self):
+    def pi_YP(self):
         self.send_command("N10 G01 Y10 F500")
 
-    # Feed movements
-    def move_feed_minus(self, step):
-        self.send_gcode(f"G91\nG0 Y-{step}\nG90\nM400")
-
-    def move_feed_plus(self, step):
-        self.send_gcode(f"G91\nG0 Y{step}\nG90\nM400")
-
-
-    def g_codes(self):    
-
-        self.btn_y_plus = QPushButton("Move Y+")
-        self.btn_y_plus.clicked.connect(lambda: ProcessAutomationController.send_command("N10 G01 Y10 F500", self.output_display))
-        #grid.addWidget(self.btn_y_plus, 0, 1)
-        
-        self.btn_x_minus = QPushButton("Move X-")
-        self.btn_x_minus.clicked.connect(lambda: ProcessAutomationController.send_command("N20 G01 X-10 F500", self.output_display))
-        #grid.addWidget(self.btn_x_minus, 1, 0)
-        
-        self.btn_home = QPushButton("Home XYZ")
-        self.btn_home.clicked.connect(lambda: ProcessAutomationController.send_command("N70 G01 X0 Y0 Z0 F200", self.output_display))
-       # grid.addWidget(self.btn_home, 1, 1)
-        
-        self.btn_x_plus = QPushButton("Move X+")
-        self.btn_x_plus.clicked.connect(lambda: ProcessAutomationController.send_command("N30 G01 X10 F500", self.output_display))
-        #grid.addWidget(self.btn_x_plus, 1, 2)
-        
-        self.btn_y_minus = QPushButton("Move Y-")
-        self.btn_y_minus.clicked.connect(lambda: ProcessAutomationController.send_command("N40 G01 Y-10 F500", self.output_display))
-        
- 
-        
-        # Z-Axis Buttons
-        self.btn_z_up = QPushButton("piMoveZPButton")
-        self.btn_z_up.clicked.connect(lambda: ProcessAutomationController.send_command("N50 G01 Z10 F500", self.output_display))
-        
-        self.btn_z_down = QPushButton("piMoveZMButton")
-        self.btn_z_down.clicked.connect(lambda: ProcessAutomationController.send_command("N60 G01 Z-10 F500", self.output_display))
-        
-        
-        self.btn_z_home = QPushButton("piHomeZButton")
-        self.btn_z_down.clicked.connect(lambda: ProcessAutomationController.send_command("N60 G01 Z0 F500", self.output_display))
 
--- src/ui/control_screen/pi_instruemnts_control_screen/pi_instruemnts_control_screen.py
+++ src/ui/control_screen/pi_instruemnts_control_screen/pi_instruemnts_control_screen.py
@@ -1,10 +1,13 @@
 from PyQt5 import uic
-from PyQt5.QtWidgets import QWidget ,QPushButton
+from PyQt5.QtWidgets import QWidget, QPushButton
+from pi_instruments.pi_intruments import pi_control
 
 class PiInstrumentsControlScreen(QWidget):
     def __init__(self, parent=None):
         super(PiInstrumentsControlScreen, self).__init__(parent)
         self.load_ui()
+        self.pi_control = pi_control()  # Create an instance of pi_control
+        self.pi_setup_connections()
 
     def load_ui(self):
         try:
@@ -14,21 +17,73 @@ class PiInstrumentsControlScreen(QWidget):
             print(f"Failed to load PiInstrumentsControlScreen UI: {e}")
 
     def pi_setup_connections(self):
-        self.findChild(QPushButton, 'piPartCleaning').clicked.connect(self)
-        self.findChild(QPushButton, 'piHomeButton').clicked.connect(self.)
-        self.findChild(QPushButton, 'piBeforeLayerStart').clicked.connect(self.)
-        self.findChild(QPushButton, 'piAfterLayerStart').clicked.connect(self.)
-        self.findChild(QPushButton, 'piBeforeVatChange').clicked.connect(self.)
-        self.findChild(QPushButton, 'piAfterVatChange').clicked.connect(self.)
-        self.findChild(QPushButton, 'Macro1Button').clicked.connect(self.)
-        self.findChild(QPushButton, 'Macro2Button').clicked.connect(self.)
+        self.findChild(QPushButton, 'piPartCleaning').clicked.connect(self.pi_part_cleaning)
+        self.findChild(QPushButton, 'piHomeButton').clicked.connect(self.pi_home)
+        self.findChild(QPushButton, 'piBeforeLayerStart').clicked.connect(self.pi_before_layer_start)
+        self.findChild(QPushButton, 'piAfterLayerStart').clicked.connect(self.pi_after_layer_start)
+        self.findChild(QPushButton, 'piBeforeVatChange').clicked.connect(self.pi_before_vat_change)
+        self.findChild(QPushButton, 'piAfterVatChange').clicked.connect(self.pi_after_vat_change)
+        self.findChild(QPushButton, 'Macro1Button').clicked.connect(self.macro1)
+        self.findChild(QPushButton, 'Macro2Button').clicked.connect(self.macro2)
 
-        self.findChild(QPushButton, 'piMoveZMButton').clicked.connect(self.)
-        self.findChild(QPushButton, 'piHomeZButton').clicked.connect(self.)
-        self.findChild(QPushButton, 'piMoveZPButton').clicked.connect(self.)
+        self.findChild(QPushButton, 'piMoveZMButton').clicked.connect(self.pi_ZM)
+        self.findChild(QPushButton, 'piHomeZButton').clicked.connect(self.pi_Zhome)
+        self.findChild(QPushButton, 'piMoveZPButton').clicked.connect(self.pi_ZP)
 
-        self.findChild(QPushButton, 'piMoveXMButton').clicked.connect(self.)
-        self.findChild(QPushButton, 'piMoveXPButton').clicked.connect(self.)
-        self.findChild(QPushButton, 'piHomeXYButton').clicked.connect(self.)
-        self.findChild(QPushButton, 'piMoveYMButton').clicked.connect(self.)
-        self.findChild(QPushButton, 'piMoveYPButton').clicked.connect(self.)
+        self.findChild(QPushButton, 'piMoveXMButton').clicked.connect(self.pi_XM)
+        self.findChild(QPushButton, 'piMoveXPButton').clicked.connect(self.pi_XP)
+        self.findChild(QPushButton, 'piHomeXYButton').clicked.connect(self.pi_XYhome)
+        self.findChild(QPushButton, 'piMoveYMButton').clicked.connect(self.pi_YM)
+        self.findChild(QPushButton, 'piMoveYPButton').clicked.connect(self.pi_YP)
+
+    def pi_ZM(self):
+        self.pi_control.pi_ZM()
+
+    def pi_Zhome(self):
+        self.pi_control.pi_Zhome()
+
+    def pi_ZP(self):
+        self.pi_control.pi_ZP()
+
+    def pi_XYhome(self):
+        self.pi_control.pi_XYhome()
+
+    def pi_XM(self):
+        self.pi_control.pi_XM()
+
+    def pi_XP(self):
+        self.pi_control.pi_XP()
+
+    def pi_YM(self):
+        self.pi_control.pi_YM()
+
+    def pi_YP(self):
+        self.pi_control.pi_YP()
+
+    # Add placeholder methods for other buttons
+    def pi_part_cleaning(self):
+        pass
+
+    def pi_home(self):
+        pass
+
+    def pi_before_layer_start(self):
+        pass
+
+    def pi_after_layer_start(self):
+        pass
+
+    def pi_before_vat_change(self):
+        pass
+
+    def pi_after_vat_change(self):
+        pass
+
+    def macro1(self):
+        pass
+
+    def macro2(self):
+        pass
+
+
+
